e5d601373b76e996e2c515a9e965a344e4379ca0
Nick <nm.richards@yahoo.com>
2016-05-12 12:02:53 -0400

    Weather API added

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-    updateTemperature();
+  updateTemperature();
   $("#powerSaving").text(thermostat.powerSavingReport());
   $("#upButton").click(function() {
     thermostat.upButton();
@@ -16,10 +16,26 @@ $(document).ready(function() {
   })
   $("#powerSavingSwitch").click(function() {
     thermostat.powerSavingSwitch();
-  $("#powerSaving").text(thermostat.powerSavingReport());
+    $("#powerSaving").text(thermostat.powerSavingReport());
   })
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.updateDisplay());
   };
- })
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#currentTemp').text(data.main.temperature);
+    })
+  }
+})
