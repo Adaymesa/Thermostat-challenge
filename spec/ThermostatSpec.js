f60bd765c55e6a9fa8dae5fe648b165c37907242
Aday Mesa <adaymesagc@gmail.com>
2016-05-12 11:40:27 +0100

    Step 5 done

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,33 +6,61 @@ describe('Thermostat', function() {
 		thermostat = new Thermostat();
 		});
 
- it('has a temperature of 20 degrees', function() {
+ it('Has a temperature of 20 degrees', function() {
  		expect(thermostat.temperature).toEqual(20);
  });
 
- it('can increase temperature when push up button', function() {
+ it('Can increase temperature when push up button', function() {
  		thermostat.upButton(1)
  		expect(thermostat.temperature).toEqual(21);
  });
 
- it('can decrease temperature when push down button', function() {
+ it('Can decrease temperature when push down button', function() {
  		thermostat.downButton(1)
  		expect(thermostat.temperature).toEqual(19);
  });
 
- it('raises an error when temperature gets 10 degrees', function() {
+ it('Raises an error when temperature gets 10 degrees', function() {
  		expect(function(){thermostat.downButton(11)}).toThrowError('It can not go below 10');
     expect(thermostat.temperature).toEqual(10);
 	});
 
-  it('has powersaving mode on by default', function(){
+  it('Has powersaving mode on by default', function(){
     expect(thermostat.powerSaving).toEqual(true);
   });
 
-  it('has powersaving mode on and raises error if temperature rises above 25', function(){
+  it('Has powersaving mode on and raises error if temperature rises above 25', function(){
   expect(function(){thermostat.upButton(6)}).toThrowError('Power saving mode is on, it cannot go above 25');
   expect(thermostat.temperature).toEqual(25);
 });
+  it('Has powersaving mode off and raises error if temperature rises above 32', function(){
+    thermostat.powerSavingMode();
+    expect(function(){thermostat.upButton(13)}).toThrowError('Power saving mode is off, it cannot go above 32');
+    expect(thermostat.temperature).toEqual(32);
+  });
 
+  it('Reset the temperature to 20 after push reset button', function() {
+    thermostat.upButton(4);
+    thermostat.resetButton();
+    expect(thermostat.temperature).toEqual(20)
+  });
+
+  it('Change the color to green when is - < 18', function() {
+    thermostat.downButton(3);
+    thermostat.checkDisplay();
+    expect(thermostat.colorDisplay).toBe('green')
+  });
+
+    it('Change the color to yellow when is between 18 and 25 degrees', function() {
+    thermostat.checkDisplay();
+    expect(thermostat.colorDisplay).toBe('yellow')
+  });
+
+    it('Change the color red when is > 25', function() {
+    thermostat.powerSavingMode();
+    thermostat.upButton(6);
+    thermostat.checkDisplay();
+    expect(thermostat.colorDisplay).toBe('red')
+  });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,13 +1,26 @@
 function Thermostat() {
 	 this.temperature = 20
 	 this.powerSaving = true
+	 this.colorDisplay = 'yellow'
 };
+
+
+Thermostat.prototype.powerSavingMode = function() {
+	this.powerSaving = !this.powerSaving
+};
+
 Thermostat.prototype.upButton = function(amount) {
 		this.temperature += amount
 		if (this.powerSaving === true){
 			if(this.temperature > 25){
 				this.temperature = 25
 				throw new Error ('Power saving mode is on, it cannot go above 25');
+			};
+		};
+		if (this.powerSaving === false){
+			if(this.temperature > 32){
+				this.temperature = 32
+				throw new Error ('Power saving mode is off, it cannot go above 32');
 			};
 		};
 
@@ -19,5 +32,23 @@ Thermostat.prototype.downButton = function(amount) {
 			this.temperature = 10
 			throw new Error ('It can not go below 10');
 		};
-
 	};
+
+Thermostat.prototype.resetButton = function() {
+		this.temperature = 20
+};
+
+Thermostat.prototype.checkDisplay = function() {
+		if (this.temperature < 18){
+			this.colorDisplay = "green"
+		};
+		if (this.temperature > 25){
+			this.colorDisplay = "red"
+		};
+};
+
+
+
+
+
+
